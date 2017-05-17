d9046b9a33f98f4126a21c7b56f458240487e6e6
DucBM <ducbm0512@gmail.com>
2017-05-17 16:21:49 +0700

    Initialize project SportBook

---
+++ SportBook/Views/MainView/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SportBook
+//
+//  Created by DucBM on 5/17/17.
+//  Copyright © 2017 dinosys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
